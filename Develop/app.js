24644477c1a88b3fe0eb5ad7884a2e8c1af15abd
AtomicZero <atomiczero@gmail.com>
2020-06-29 01:28:05 +0100

    homework 10 final

--- Develop/app.js
+++ Develop/app.js
@@ -10,9 +10,135 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+const firstQuestions = [
+    {
+        name: 'name',
+        message: 'enter your full name',
+        type: 'input',
+    },
+    {
+        name: 'id',
+        message: 'enter your employee id',
+        type: 'input',
+    },
+    {
+        name: 'email',
+        message: 'enter your email',
+        type: 'input',
+    },
+    {
+        name: 'role',
+        message: 'enter your job role',
+        type: 'list',
+        choices: [
+
+            {
+                name: 'Engineer',
+                value: "engineer",
+                short: "Engineer"
+            },
+            {
+                name: 'Manager',
+                value: "manager",
+                short: "Manager"
+            },
+            {
+                name: 'Intern',
+                value: "intern",
+                short: "Intern"
+            },
+        ],
+
+    },
+
+];
+
+const askAgain = [
+    {
+        name: "isContinue",
+        message: "Do you want to add another Employee?",
+        type: "list",
+        choices: [
+            {
+                name: 'Yes',
+                value: 'yes',
+                short: "Yes"
+            },
+            {
+                name: 'No',
+                value: 'no',
+                short: "No"
+            }
+        ]
+    }
+]
+
+const employeesArray = []
+
+const checkAndAsk = () => {
+    inquirer.prompt(askAgain).then(async (answers) => {
+        if (answers.isContinue === 'yes') {
+            await init()
+        } else {
+            console.log('Stop')
+            console.log(employeesArray)
+            fs.writeFile(outputPath, render(employeesArray), function (err) {
+                if (err) throw err;
+                console.log('HTML FILE GENERATED')
+            })
+        }
+    })
+}
+
+async function init() {
+    const answers = await inquirer.prompt(firstQuestions)
+
+    if (answers.role === "engineer") {
+        const engineerQuestions = [
+            {
+                name: 'github',
+                message: 'enter your git link',
+                type: 'input',
+            },
+        ]
+        const engineerAnswers = await inquirer.prompt(engineerQuestions)
+        const engineer = new Engineer(answers.name, answers.id, answers.email, engineerAnswers.github)
+        employeesArray.push(engineer)
+        checkAndAsk()
+    } else if (answers.role === "manager") {
+        const managerQuestions = [
+            {
+                name: 'officeNumber',
+                message: 'enter your office number',
+                type: 'input',
+            },
+        ]
+        const managerAnswers = await inquirer.prompt(managerQuestions)
+        const manager = new Manager(answers.name, answers.id, answers.email, managerAnswers.officeNumber)
+        employeesArray.push(manager)
+        checkAndAsk()
+    } else if (answers.role === "intern") {
+        const internQuestions = [
+            {
+                name: 'university',
+                message: 'enter your university',
+                type: 'input',
+            },
+        ]
+        const internAnswers = await inquirer.prompt(internQuestions)
+        const intern = new Intern(answers.name, answers.id, answers.email, internAnswers.university)
+        employeesArray.push(intern)
+        checkAndAsk()
+    } else {
+        console.log("Invalid choice here")
+    }
+}
+//Calling of function that runs in node terminal
+init();
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
